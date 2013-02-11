0d2b5ce04e63465f6e27c2af85b3277f689399e0
noonius <tanel@noonius.net>
2013-02-11 14:53:49 +0200

    Update rails-install-ubuntu.sh

--- rails-install-ubuntu.sh
+++ rails-install-ubuntu.sh
@@ -14,14 +14,14 @@ curl -L get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
 
 echo "Installs Ruby"
-rvm install 1.9.3-p194
-rvm use 1.9.3-p194 --default
+rvm install 1.9.3
+rvm use 1.9.3 --default
 
 gem install bundler --no-rdoc --no-ri
 gem install rails --no-rdoc --no-ri
 
-echo "Installs text editor"
-sudo apt-get install gedit -y
+#echo "Installs text editor"
+#sudo apt-get install gedit -y
 
 echo -e "\n- - - - - -\n"
 echo -e "Now we are going to print some information to check that everything is done:\n"
@@ -34,8 +34,8 @@ echo -n "Should be sqlite 3.7.3 or higher: sqlite "
 sqlite3 --version
 echo -n "Should be rvm 1.6.32 or higher:          "
 rvm --version | sed '/^.*$/N;s/\n//g' | cut -c 1-10
-echo -n "Should be ruby 1.9.3-p194:                "
-ruby -v | cut -d " " -f 2
+#echo -n "Should be ruby 1.9.3-p194:                "
+#ruby -v | cut -d " " -f 2
 echo -n "Should be Rails 3.2.2 or higher:         "
 rails -v
 echo -e "\n- - - - - -\n"
